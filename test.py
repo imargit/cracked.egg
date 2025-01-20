1da077f9ecf7e6dd39fe4b1cf699282bc640cebb
Imar Sibbing <imarsibbing@gmail.com>
2025-01-20 15:22:05 +0100

    added republican/democrats

--- test.py
+++ test.py
@@ -6,16 +6,24 @@ for letter in alphabet:
     with open(f'Dataset\{letter}_people.json', encoding='utf-8') as file:
         People.append(json.load(file))
  
-#create empty list
+#create empty lists
 republican_list = [] 
+democrat_list = []
+
+for letter in People:
+    for item in letter:
+        if 'ontology/party_label' in item:
+            if item['ontology/party_label'] == 'Republican Party (United States)': 
+                republican_list.append(item)
 
 for letter in People:
     for item in letter:
         if 'ontology/party_label' in item:
             if item['ontology/party_label'] == 'Democratic Party (United States)': 
-                republican_list.append(item)
+                democrat_list.append(item)
         
-print(len(republican_list))
+print(f"Republicans: {len(republican_list)}")
+print(f"Democrats: {len(democrat_list)}")
 
 
 
