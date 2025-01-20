a386631d589ba7eced786798fa8e9af768081c25
Agoston Csehi <csehi.agoston.benedek@gmail.com>
2025-01-20 15:27:59 +0100

    cleaned up

--- test.py
+++ test.py
@@ -15,10 +15,6 @@ for letter in People:
         if 'ontology/party_label' in item:
             if item['ontology/party_label'] == 'Republican Party (United States)': 
                 republican_list.append(item)
-
-for letter in People:
-    for item in letter:
-        if 'ontology/party_label' in item:
             if item['ontology/party_label'] == 'Democratic Party (United States)': 
                 democrat_list.append(item)
         
