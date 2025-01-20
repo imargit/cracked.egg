875d5938dc66d8adef0f75186b05f8f1100ef2de
Imar Sibbing <imarsibbing@gmail.com>
2025-01-20 16:28:34 +0100

    Cleaned merge

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import json
 current_dir = Path()
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet = ['A']
+            #, 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 #open file
 people_all=[]
 for letter in alphabet:
