26a4936d2ea2a2ea7e640baaae552d0a791b621d
Imar Sibbing <imarsibbing@gmail.com>
2025-01-20 15:07:05 +0100

    Going over every datafile

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import json
-
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 #open file
-with open('A_people.json', encoding='utf- 8') as file:
-    People = json.load(file) 
+People=[]
+for letter in alphabet:
+    with open(f'Dataset\{letter}_people.json', encoding='utf-8') as file:
+        People.append(json.load(file))
  
 #create empty list
 republican_list = [] 
 
-#measurements for seattle
-for item in People:
-    if 'ontology/party_label' in item:
-        if item['ontology/party_label'] == 'Democratic Party (United States)': 
-            republican_list.append(item)
+for letter in People:
+    for item in letter:
+        if 'ontology/party_label' in item:
+            if item['ontology/party_label'] == 'Democratic Party (United States)': 
+                republican_list.append(item)
         
 print(len(republican_list))
 
