f4a2fb574a8ee048af6ff3264a1f494bf61eae80
Imar Sibbing <imarsibbing@gmail.com>
2025-01-22 15:44:04 +0100

    added filters/cut-off, beautified visualisation

--- endowments.R
+++ endowments.R
@@ -1,16 +1,18 @@
 library(tidyverse)
 library(jsonlite)
+library(scales) 
 
-edowment <- read_csv('university_characteristics.csv') 
+# Cut-off value: At least this number of republicans or democrats have attended this university
+cut_off = 10
+# Cut-off value (%): There is at least this percentage MORE democrats/republicans (100% = same amount)
+cut_off_surplus = 150
 
-cut_off = 1
-
-repu_df <- as.data.frame(fromJSON('republican.json', flatten = TRUE)) |>
+repu_df <- as.data.frame(fromJSON('Data/republican.json', flatten = TRUE)) |>
   pivot_longer(cols = everything(), names_to = 'Universities', values_to = 'Frequency')|>
   mutate(University = str_replace_all(Universities, '\\.',' ')) |>
   select(University,Frequency)
 
-demo_df <- as.data.frame(fromJSON('democrat.json', flatten = TRUE)) |>
+demo_df <- as.data.frame(fromJSON('Data/democrat.json', flatten = TRUE)) |>
   pivot_longer(cols = everything(), names_to = 'Universities', values_to = 'Frequency')|>
   mutate(University = str_replace_all(Universities, '\\.',' ')) |>
   select(University,Frequency)
@@ -19,24 +21,20 @@ merged_df <- merge(repu_df,demo_df, by = 'University') |>
   rename(repu_freq = Frequency.x, demo_freq = Frequency.y)
 
 df_3 <- merged_df |>
-  filter(repu_freq > cut_off | demo_freq > cut_off) |>
   mutate(surplus_demo = demo_freq - repu_freq) |>
   mutate(surplus_demo_grouping = surplus_demo > 0) |> 
   group_by(surplus_demo_grouping) |>
-  rename(institution_name = University)
+  rename(institution_name = University) |>
+  filter(repu_freq > cut_off | demo_freq > cut_off) |>
+  filter((demo_freq/repu_freq)*100 > cut_off_surplus | (repu_freq/demo_freq)*100 > cut_off_surplus) 
 print(df_3)
 
-merged_df <- merge(repu_df,demo_df, by = 'University') |>
-  rename(repu_freq = Frequency.x, demo_freq = Frequency.y) 
-
-
-endowment_person <- read_csv('graduation.csv') |>
+endowment_person <- read_csv('Data/graduation.csv') |>
   group_by(year) |>
   filter(year == 2018)
-print(endowment_person)
 
 merged_data <-  merge(df_3, endowment_person, by = 'institution_name') 
-  #create a new column with frequencies for the party that the university has a surplus of
+#create a new column with frequencies for the party that the university has a surplus of
 #mutate(freq_party = case_when(surplus_demo_grouping == "TRUE", demo_freq | surplus_demo_grouping == "FALSE", repu_freq))
   
 
@@ -44,21 +42,19 @@ merged_data <-  merge(df_3, endowment_person, by = 'institution_name')
 summarized_merged <- merged_data |>
   group_by(surplus_demo_grouping) |>
   summarize(mean = mean(endowment_pp, na.rm = TRUE))
-print(summarized_merged)
+
 
 #create barplot
 plot_endowment <- ggplot(data = summarized_merged) +
   aes(x = surplus_demo_grouping, y = mean)  +
-  geom_col() +
-  labs(x = 'Party', y = 'Endowment per student')
-
+  geom_col(fill=c("red","blue")) +
+  labs(x = 'University affiliation', 
+       y = 'Endowment per student ($)')+
+  scale_x_discrete(labels = c('Republican','Democrat'))+
+  scale_y_continuous(labels = scales::comma)
+ggsave('figures/endowment_per_university_affiliation.pdf', width = 7, height = 5)
 
 #create scatterplot
 # x = amount of students
 # y = edowment pp
-
-
-
-
-
 print(plot_endowment)
